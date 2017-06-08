228a7aab2b42a14352e0886ef46f5e4f6e5da8ec
Mandar Deshpande2 <Mandar_Deshpande2@intuit.com>
2017-06-08 13:05:32 -0700

    changes from child branch

--- app/js/components/UserName.jsx
+++ app/js/components/UserName.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class UserName extends Component{
   render(){
-    return <h3>Hello new User!</h3>
+    return (
+      <div>
+        <h3>Hello new User!</h3>
+        <p>Adding some more changes to code from child branch...</p>
+      </div>
+    )
   }
 }
